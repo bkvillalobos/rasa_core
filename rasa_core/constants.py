e3090fdd38ace6042998cea353041c627b56349a
Brendon Villalobos <brendonv93@gmail.com>
2018-08-31 22:45:15 -0400

    set default port 8080

--- rasa_core/constants.py
+++ rasa_core/constants.py
@@ -3,7 +3,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-DEFAULT_SERVER_PORT = 5005
+DEFAULT_SERVER_PORT = 8080
 
 DEFAULT_SERVER_URL = "http://localhost:{}".format(DEFAULT_SERVER_PORT)
 
